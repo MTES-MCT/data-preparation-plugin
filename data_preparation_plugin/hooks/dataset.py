fb9be8fde0585838d53bdfd2ad1e7f5379ebb0d6
benoitguigal <benoit.guigal@gmail.com>
2019-08-07 10:04:41 +0200

    implement iter_rows, speed up bulk loading and handle reflection when no primary key is defined

--- data_preparation_plugin/hooks/dataset.py
+++ data_preparation_plugin/hooks/dataset.py
@@ -22,10 +22,9 @@ class PostgresDatasetWriter(object):
         return self
 
     def __exit__(self, *args):
-        Model = self.dataset.reflect()
-        session = self.dataset.get_session()
-        session.bulk_insert_mappings(Model, self.rows)
-        session.commit()
+        Mapper = self.dataset.reflect()
+        engine = self.dataset.engine
+        engine.execute(Mapper.__table__.insert(), self.rows)
 
     def write_row_dict(self, row):
         self.rows.append(row)
@@ -53,27 +52,40 @@ class PostgresDataset(PostgresHook):
         super().__init__(*args, **kwargs)
         self.pg_schema = schema
         self.name = name
-        self.engine = self.get_sqlalchemy_engine()
+        engine_kwargs = {"use_batch_mode": True}
+        self.engine = self.get_sqlalchemy_engine(engine_kwargs=engine_kwargs)
         self.mapper = None
 
-    def reflect(self, force=False):
+    def reflect(self, force=False, primary_key=None):
         """
         This function allow to reflect the schema of an existing
         table, returning an sqlalchemy ORM mapped class to work with
         """
         def _reflect():
+
+            table = Table(
+                self.name,
+                Base.metadata,
+                schema=self.pg_schema,
+                autoload=True,
+                autoload_with=self.engine)
+
             class Mapper(Base):
-                __table__ = Table(
-                    self.name,
-                    Base.metadata,
-                    schema=self.pg_schema,
-                    autoload=True,
-                    autoload_with=self.engine)
+                __table__ = table
+
+                # See https://docs.sqlalchemy.org/en/13/faq/ormconfiguration.html
+                # #how-do-i-map-a-table-that-has-no-primary-key
+                __mapper_args__ = {
+                    'primary_key': [getattr(table.c, primary_key)]
+                } if primary_key else {}
+
             return Mapper
 
         if force:
             self.mapper = None
+
         self.mapper = self.mapper or _reflect()
+
         return self.mapper
 
     def get_dataframe(self, sql=None):
@@ -85,19 +97,36 @@ class PostgresDataset(PostgresHook):
                     name=self.name)
         return self.get_pandas_df(sql)
 
-    def iter_rows(self):
-        raise NotImplemented()
+    def iter_rows(self, head=None):
+        """
+        Returns an iterator over all the rows of the datatset
+        represented as dictionnary of (column, value) pairs
+        """
+        def row2dict(row):
+            d = {}
+            for column in row.__table__.columns:
+                d[column.name] = getattr(row, column.name)
+            return d
+
+        Mapper = self.reflect()
+        session = self.get_session()
+        q = session.query(Mapper)
+        if head:
+            q = q.limit(head)
+        rows = [row2dict(r) for r in q.all()]
+        session.close()
+        return rows
 
     def get_writer(self):
         """ Returns a writer to the dataset using a context manager """
         return PostgresDatasetWriter(self)
 
-    def read_dtype(self):
+    def read_dtype(self, primary_key=None):
         """
         Returns the data type of the dataset as a list
         of SQL Alchemy colums
         """
-        Mapper = self.reflect()
+        Mapper = self.reflect(primary_key=primary_key)
         columns = Mapper.__table__.columns
         return [column.copy() for column in columns]
 
@@ -122,21 +151,16 @@ class PostgresDataset(PostgresHook):
         Session = sessionmaker()
         return Session(bind=self.engine)
 
-    # TODO delete
     def write_from_dataframe(self, df, dtype=None):
         """
         Write the dataframe to the dataset, overwriting
         previous data. The schema of the dataframe must match
         the schema of the dataset
         """
-
         engine_kwargs = {"use_batch_mode": True}
         engine = self.get_sqlalchemy_engine(engine_kwargs)
 
         # TODO this is slow
-        # Check theis solution
-        # https://www.codementor.io/bruce3557/
-        # graceful-data-ingestion-with-sqlalchemy-and-pandas-pft7ddcy6
         df.to_sql(
             self.name,
             engine,
@@ -145,23 +169,3 @@ class PostgresDataset(PostgresHook):
             index=True,
             dtype=dtype)
 
-    # TODO delete
-    def left_join(self, dataset, join_key_a, join_key_b):
-        """
-        Perform an SQL join with another dataset
-        """
-        fmt_params = {
-            "table_name": self.table_name,
-            "join_with": dataset.table_name,
-            "join_key_a": join_key_a,
-            "join_key_b": join_key_b
-        }
-        sql = textwrap.dedent("""
-            SELECT * from {table_name} as a
-            LEFT JOIN {join_with} as b
-            ON a.{join_key_a} = b.{join_key_b}
-            """.format(**fmt_params))
-        df = self.get_pandas_df(sql)
-        dtype = {**self.dtype, **dataset.dtype}
-
-        return (df, dtype)
--- data_preparation_plugin/hooks/tests/test_dataset.py
+++ data_preparation_plugin/hooks/tests/test_dataset.py
@@ -31,13 +31,13 @@ class DatasetTestCase(TestCase):
 
         db = PostgresHook("postgres_test")
         create_schema = textwrap.dedent("""CREATE SCHEMA IF NOT EXISTS test""")
-        db.run(create_schema, autocommit=True)
+        db.run(create_schema)
 
     @classmethod
     def tearDownClass(cls):
         db = PostgresHook("postgres_test")
         drop_schema = textwrap.dedent("""DROP SCHEMA IF EXISTS test CASCADE""")
-        db.run(drop_schema, autocommit=True)
+        db.run(drop_schema)
 
     def setUp(self):
         db = PostgresHook("postgres_test")
@@ -48,7 +48,7 @@ class DatasetTestCase(TestCase):
                 foo VARCHAR(255)
             );
             """)
-        db.run(create_table, autocommit=True)
+        db.run(create_table)
         self.dataset = PostgresDataset(
             name="test",
             schema="test",
@@ -58,12 +58,36 @@ class DatasetTestCase(TestCase):
         db = PostgresHook("postgres_test")
         drop_table = textwrap.dedent("""
             DROP TABLE IF EXISTS test.test""")
-        db.run(drop_table, autocommit=True)
+        db.run(drop_table)
 
     def test_reflect(self):
         # it should return an ORM class
         Foo = self.dataset.reflect()
         self.assertEqual(Foo.__table__.name, "test")
+
+    def test_reflect_no_primary_key(self):
+        db = PostgresHook("postgres_test")
+
+        # create a table with no primary key but with
+        # some id which should be unique
+        create_table = textwrap.dedent("""
+            CREATE TABLE IF NOT EXISTS test.no_primary_key (
+                some_id INTEGER,
+                foo VARCHAR(255)
+            );
+            """)
+        db.run(create_table, autocommit=True)
+        dataset = PostgresDataset(
+            name="no_primary_key",
+            schema="test",
+            postgres_conn_id="postgres_test")
+
+        Foo = dataset.reflect(primary_key="some_id")
+        self.assertEqual(Foo.__table__.name, "no_primary_key")
+
+        drop_table = textwrap.dedent("""
+            DROP TABLE IF EXISTS test.no_primary_key""")
+        db.run(drop_table, autocommit=True)
 
     def test_writer(self):
         rows = [{"foo": "bar1"}, {"foo": "bar2"}]
@@ -92,3 +116,18 @@ class DatasetTestCase(TestCase):
         new_dtype = self.dataset.read_dtype()
         self.assertEqual(len(new_dtype), 3)
         self.assertEqual(new_dtype[2].name, "new_column")
+
+    def test_iter_rows(self):
+        insert = textwrap.dedent("""
+            INSERT INTO test.test (foo) VALUES ('foo1'), ('foo2')
+        """)
+        db = PostgresHook("postgres_test")
+        db.run(insert)
+        rows = []
+        for row in self.dataset.iter_rows():
+            rows.append(row)
+        self.assertEqual(len(rows), 2)
+        expected = [
+            {'id': 1, 'foo': 'foo1'},
+            {'id': 2, 'foo': 'foo2'}]
+        self.assertEqual(rows, expected)
