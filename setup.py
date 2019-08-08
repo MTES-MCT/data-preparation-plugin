df277f447eae1c91895805f84435ba36ee7423d8
benoitguigal <benoit.guigal@gmail.com>
2019-08-08 14:22:00 +0200

    update setup.py

--- setup.py
+++ setup.py
@@ -1,11 +1,18 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
     name="data_preparation_plugin",
-    version='1.0',
+    version='0.1',
+    description="Airflow plugin with database hooks and" +
+                "operators for common data preparation tasks",
     entry_points={
-        'airflow.plugins': [
-            'data_preparation_plugin = data_preparation_plugin.data_preparation_plugin:DataPreparationPlugin'
+        "airflow.plugins": [
+            "data_preparation_plugin = " +
+            "data_preparation_plugin.data_preparation_plugin:" +
+            "DataPreparationPlugin"
         ]
-    }
+    },
+    author_email="benoit.guigal@protonmail.com",
+    packages=find_packages(exclude=['contrib', 'docs', 'tests']),  # Required
+    python_requires=">=3.6"
 )
